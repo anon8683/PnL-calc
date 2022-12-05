17f51ab15cc01ad0bed05a6f5267a05f76ab86bf
Joe <flightbuzz1234@gmail.com>
2022-12-05 19:08:16 +0000

    long/short working, need to fix pnl%

--- javascript.js
+++ javascript.js
@@ -3,14 +3,14 @@ const inputLeverage = document.querySelector(".lev");
 const qty = document.querySelector("#qty");
 const entry = document.querySelector("#entry");
 const collateral = document.querySelector(".collateral");
-const close = document.querySelector("#close");
+const closes = document.querySelector("#close");
 const profitLoss = document.querySelector("#profit");
 const profitP = document.querySelector("#profitP");
 const roi = document.querySelector("#roi");
 const shortButton = document.querySelector(".short");
 const longButton = document.querySelector(".long");
 
-let leverage = 1;
+let leverage = 0;
 let quantity = 0;
 let entryPrice = 0;
 let closePrice = 0;
@@ -25,6 +25,7 @@ longButton.addEventListener("click", (e) => {
   shortButton.classList.remove("buttonBorder");
   long = true;
   short = false;
+  pnl();
 });
 
 shortButton.addEventListener("click", (e) => {
@@ -33,38 +34,38 @@ shortButton.addEventListener("click", (e) => {
 
   long = false;
   short = true;
+  pnl();
 });
 
 slider.addEventListener("input", (e) => {
   leverage = slider.value;
   inputLeverage.value = slider.value;
   margin();
-  pnl();
+  checkClose();
 });
 
 inputLeverage.addEventListener("change", (e) => {
   leverage = +inputLeverage.value;
   slider.value = +inputLeverage.value;
   margin();
-  pnl();
+  checkClose();
 });
 
 qty.addEventListener("change", (e) => {
   quantity = +qty.value;
   margin();
-  pnl();
+  checkClose();
 });
 
 entry.addEventListener("change", (e) => {
   entryPrice = +entry.value;
   margin();
-  pnl();
+  checkClose();
 });
 
-close.addEventListener("change", (e) => {
-  closePrice = +close.value;
-  margin();
-  pnl();
+closes.addEventListener("change", (e) => {
+  closePrice = +closes.value;
+  checkClose();
 });
 
 function margin() {
@@ -75,10 +76,33 @@ function margin() {
 }
 
 function pnl() {
-  realPnl = +(quantity * closePrice - quantity * entryPrice).toFixed(1);
+  if (long === true) {
+    realPnl = +(quantity * closePrice - quantity * entryPrice).toFixed(1);
+    profitLoss.textContent = `${realPnl} USDT`;
+    profitP.textContent = `${((realPnl / entryPrice) * 100).toFixed(2)}%`;
+    roi.textContent = `${((realPnl / iniMargin) * 100).toFixed(2)}%`;
+    return;
+  }
+
+  realPnl = +(quantity * entryPrice - quantity * closePrice).toFixed(1);
+
   profitLoss.textContent = `${realPnl} USDT`;
   profitP.textContent = `${((realPnl / entryPrice) * 100).toFixed(2)}%`;
   roi.textContent = `${((realPnl / iniMargin) * 100).toFixed(2)}%`;
 
   return;
+}
+
+function checkClose() {
+  if (closePrice > 0) {
+    pnl();
+  } else {
+    defaults();
+  }
+}
+
+function defaults() {
+  profitLoss.textContent = "0.00 USDT";
+  profitP.textContent = "0%";
+  roi.textContent = "0%";
 }
