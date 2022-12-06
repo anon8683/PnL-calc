b1c9665f3b44e142aa5784a41c2d870592f4b467
Joe <flightbuzz1234@gmail.com>
2022-12-06 09:04:01 +0000

    added liq price, weird interaction between close price and liq price, will fix

--- javascript.js
+++ javascript.js
@@ -9,6 +9,7 @@ const profitP = document.querySelector("#profitP");
 const roi = document.querySelector("#roi");
 const shortButton = document.querySelector(".short");
 const longButton = document.querySelector(".long");
+const liquidation = document.querySelector("#liq");
 
 let leverage = 0;
 let quantity = 0;
@@ -16,6 +17,8 @@ let entryPrice = 0;
 let closePrice = 0;
 let iniMargin = 0;
 let realPnl = 0;
+let liq = 0;
+let orderValue = 0;
 
 let long = true;
 let short = false;
@@ -25,7 +28,8 @@ longButton.addEventListener("click", (e) => {
   shortButton.classList.remove("buttonBorder");
   long = true;
   short = false;
-  pnl();
+  checkClose();
+  liqPrice();
 });
 
 shortButton.addEventListener("click", (e) => {
@@ -34,7 +38,8 @@ shortButton.addEventListener("click", (e) => {
 
   long = false;
   short = true;
-  pnl();
+  checkClose();
+  liqPrice();
 });
 
 slider.addEventListener("input", (e) => {
@@ -42,6 +47,7 @@ slider.addEventListener("input", (e) => {
   inputLeverage.value = slider.value;
   margin();
   checkClose();
+  liqPrice();
 });
 
 inputLeverage.addEventListener("change", (e) => {
@@ -49,6 +55,7 @@ inputLeverage.addEventListener("change", (e) => {
   slider.value = +inputLeverage.value;
   margin();
   checkClose();
+  liqPrice();
 });
 
 qty.addEventListener("change", (e) => {
@@ -61,6 +68,7 @@ entry.addEventListener("change", (e) => {
   entryPrice = +entry.value;
   margin();
   checkClose();
+  liqPrice();
 });
 
 closes.addEventListener("change", (e) => {
@@ -69,7 +77,7 @@ closes.addEventListener("change", (e) => {
 });
 
 function margin() {
-  iniMargin = +((quantity * entryPrice) / leverage).toFixed(1);
+  iniMargin = +((quantity * entryPrice) / leverage).toFixed(2);
   collateral.textContent = `${((quantity * entryPrice) / leverage).toFixed(
     1
   )} USDT`;
@@ -81,16 +89,39 @@ function pnl() {
     profitLoss.textContent = `${realPnl} USDT`;
     profitP.textContent = `${((realPnl / entryPrice) * 100).toFixed(2)}%`;
     roi.textContent = `${((realPnl / iniMargin) * 100).toFixed(2)}%`;
+    liquidation.textContent = `${(
+      quantity * entryPrice -
+      iniMargin +
+      (iniMargin / 100) * 1.6667
+    ).toFixed(2)} USDT`;
+    checkColor();
+
     return;
   }
 
   realPnl = +(quantity * entryPrice - quantity * closePrice).toFixed(1);
-
   profitLoss.textContent = `${realPnl} USDT`;
   profitP.textContent = `${((realPnl / entryPrice) * 100).toFixed(2)}%`;
   roi.textContent = `${((realPnl / iniMargin) * 100).toFixed(2)}%`;
+  checkColor();
 
   return;
+}
+
+//Calcs our liquidation price based on leverage,quantity,entry price
+function liqPrice() {
+  if (leverage > 0 && quantity > 0 && entryPrice > 0) {
+    orderValue = entryPrice * quantity;
+
+    if (long === true) {
+      //   liq = liq + (iniMargin / 100) * 5;
+      liq = (orderValue - iniMargin) / quantity;
+      liquidation.textContent = `${liq.toFixed(2)} USDT`;
+    } else {
+      liq = (orderValue + iniMargin) / quantity;
+      liquidation.textContent = `${liq.toFixed(2)} USDT`;
+    }
+  }
 }
 
 function checkClose() {
@@ -105,4 +136,22 @@ function defaults() {
   profitLoss.textContent = "0.00 USDT";
   profitP.textContent = "0%";
   roi.textContent = "0%";
+}
+
+function checkColor() {
+  if (realPnl > 0) {
+    profitLoss.classList.remove("loss");
+    profitP.classList.remove("loss");
+    roi.classList.remove("loss");
+    profitLoss.classList.add("profit");
+    profitP.classList.add("profit");
+    roi.classList.add("profit");
+  } else {
+    profitLoss.classList.remove("profit");
+    profitP.classList.remove("profit");
+    roi.classList.remove("profit");
+    profitLoss.classList.add("loss");
+    profitP.classList.add("loss");
+    roi.classList.add("loss");
+  }
 }
